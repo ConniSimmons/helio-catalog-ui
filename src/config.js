306b01a2c92e34d91b0ad97399434766b0cbfeec
Conni Simmons <connisimmons@gmail.com>
2020-07-18 17:40:37 -0600

    changing to local servers for Brian's use

--- src/config.js
+++ src/config.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
+/*
 export default axios.create({
   baseURL: "https://helio-catalog-api.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
 });
+*/
 
-/*
 export default axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
 });
-*/
+
